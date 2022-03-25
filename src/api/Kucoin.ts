f3ca37f46a97ada234b591cbea3c0cc4e24f5d71
Anatolii Yarovyi <69160081+AnatoliiYarovyi@users.noreply.github.com>
2022-03-25 12:11:35 +0200

    Update Kucoin.ts

--- src/api/Kucoin.ts
+++ src/api/Kucoin.ts
@@ -1,16 +1,22 @@
-import axios from 'axios';
+import axios, { AxiosResponse } from 'axios';
+
+import { Obj, DetailsCoin } from '../interface/interface';
+interface DataCoins {
+  symbol: string;
+  price: string;
+}
 
 const endPoints = 'https://api.kucoin.com/api/v1/prices';
 
 const getArrKucoin = async () => {
-  const resData = await axios
+  const resData: DetailsCoin[] = await axios
     .get(endPoints)
-    .then(resp => {
+    .then((resp: AxiosResponse) => {
       // we take object {} from the response with key="crypt name", value="price"
-      const objCoins = resp.data.data;
+      const objCoins: Obj = resp.data.data;
 
       // using "for in" iterate over objCoins and create the array of objects we need
-      const dataCoins = [];
+      const dataCoins: DataCoins[] = [];
       for (const key in objCoins) {
         dataCoins.push({
           symbol: key,
@@ -20,15 +26,15 @@ const getArrKucoin = async () => {
 
       return dataCoins.reduce((acc, el, i) => {
         acc.push({
-          shop_name: 'kucoin',
-          list_number: i + 1,
+          shopName: 'kucoin',
+          listNumber: i + 1,
           symbol: el.symbol,
           price: Number(el.price),
         });
         return acc;
       }, []);
     })
-    .catch(error => console.log(error));
+    .catch(error => console.error(error));
   return resData;
 };
 
